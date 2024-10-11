f38597db405a3c4a040826fc6700d0dd55229ced
YossraNour1996 <yossranour1996@gmail.com>
2024-10-11 08:31:21 +0400

    add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Zomato Chatbot",
+    version= "0.0.0",
+    author= "Yossra Nour",
+    author_email= "yossnour96@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
